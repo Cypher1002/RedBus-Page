448b77b43ad11892e28708b0919932768980d246
Mehul <mehul.darshni@accionlabs.com>
2023-08-23 09:46:32 +0530

    features added

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ let details=[];
 const data1  = document.getElementById("data-to");
 const data2 =  document.getElementById("data-from")
 const text =  document.getElementById('msgdisplay');
+const toErr =  document.getElementById('to-err');
+const fromErr = document.getElementById('from-err');
+const dateErr =  document.getElementById('date-err');
+
 
 const addDetails = (e) =>{
     e.preventDefault();
@@ -10,8 +14,25 @@ const addDetails = (e) =>{
         To: document.getElementById('to').value,
         Date: document.getElementById('date').value
     }
-    if(detail.To != "" && detail.from != "" && detail.Date!="")
-        details.push(detail);
+         
+       if(detail.from =="" && detail.from=="" && detail.Date ==""){
+             !details.push(detail)
+             fromErr.innerHTML ="*Enter To from and Date";
+             fromErr.style.color = "red"
+     }   
+
+       if(!details.some(existingDetails => existingDetails.from === detail.from
+         &&
+           existingDetails.To === detail.To &&
+           existingDetails.Date === detail.Date)){
+           details.push(detail);
+        showCard();
+       }
+
+
+    // if(!details.includes(detail))
+    //     details.push(detail);
+
     //  document.forms[0].reset();
 
     //  console.log({details})
@@ -20,27 +41,57 @@ const addDetails = (e) =>{
     //     data2.innerText = details[i].To;
     //     console.log(details[i].Date);
     // }
-    if( detail.To != "" && detail.from != "" && detail.Date!=""){
-      showCard();
-    }else if(detail.from == ""){
-       text.innerHTML ="enter your origin city";
-    }else if(detail.To == ""){
-        text.innerHTML = "Enter destination city";
-    }else if(detail.Date == ""){
-        text.innerHTML ="Enter a valid Date";
-    }
-    else{
-        text.innerHTML = "All feilds are nessesary";
-    }
-   
-}
 
+    // console.log(detail)
+    // if(detail.To != "" && detail.from != "" && detail.Date!=""){
+    //     // text.innerHTML = "";
+    //   showCard();
+    // }
+   
+
+    if(detail.from == "" && detail.To=="" && detail.Date==""){
+        fromErr.innerHTML ="*Enter To from and Date";
+       fromErr.style.color = "red"
+    }else{
+        fromErr.innerHTML =""
+    }
+    
+    if(detail.To == "" && detail.Date== "" ){
+        fromErr.innerHTML = "*enetr to and date ";
+        fromErr.style.color = "red"
+    }else{
+        toErr.innerHTML =" "
+    }
+    
+    // if(detail.Date == ""){
+    //     fromErr.innerHTML ="*Enter a valid Date";
+    //     dateErr.style.color = "red"
+
+    // }else{
+    //     dateErr.innerHTML =" ";
+    // }
+  
+    //  validFrom();
+}
+   
+
+function prev(){
+    if(details != 0){
+        document.getElementById("psearch").innerText ="PREVIOUS SEARCHES";
+    }else{
+        document.getElementById("psearch").innerHTML ="";
+    }
+}
 function showCard(){
+
+    prev();
+
     let html = "";
+
+
 
     for(var i = 0 ;i<details.length;i++){
         html +=`
-        <div class="Ffirst">
         <div class="Bblock">
           <p>
           <h3 id="data-to">to :${details[i].from}</h3>
@@ -48,14 +99,22 @@ function showCard(){
            <h3>Date: ${details[i].Date} </h3>
            </p>
         </div>
-      </div>
         `
     }
-   
-    let datael = document.getElementById('firstOne');
+       
+     let datael = document.getElementById('firstOne');
     datael.innerHTML= html;
+ 
+        console.log(details)
 
 }
+
+{/* <button id="next" onclick="next()"> next </button> */}
+
+function next(){
+   let  block1 = document.getElementsByClassName('firstOne');
+   block1 += 3;
+  }
 
 
 document.addEventListener('DOMContentLoaded', ()=>{
@@ -77,12 +136,6 @@ function myFunction(){
 }
 
 
-function validForm(){
-    if (a.value == " ") {
-        document.getElementById('msgdisplay').innerHTML = "please rite name";
-    }
-}
-
 
     var Today = new Date();  //js inbuilt function
     var day = Today.getDate();
@@ -98,3 +151,35 @@ function validForm(){
     document.getElementById('date').setAttribute('min', minDate);
 
     console.log(Today)
+
+
+// function setError(id, error){
+//      element= document.getElementById(id)
+//     //  Element.innerHTML = error;
+//       element.getElementsByClassName('msgdisplay').innerHTML = error
+
+// }
+
+// function validFrom(){
+//     var fname = document.forms['myForm']['from1'].value;
+//     if(fname == ""){
+//         setError("fname", "enetr from detail")
+//     }
+
+//     var fTo =  document.froms['myForm']['to1'].value;
+//     if(fTo == ""){
+//         setError("fTo", "enter To details")
+//     }
+
+//     console.log(fTo)
+// }
+
+
+function clr(){
+    details = [];
+    console.log(details);
+
+    showCard();
+    prev();
+}
+
